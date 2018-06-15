79df6503f53f8dff6b03ebe56a8c86688f896651
lawlietzone <84158090@qq.com>
2018-06-15 00:29:13 -0400

    fix assignment

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,8 +3,11 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,8 @@ public class DetailActivity extends AppCompatActivity {
     TextView placeOfOriginTv;
     TextView descriptionTv;
     Sandwich sandwich;
+    LinearLayout also_known_linearLayout;
+    LinearLayout ingredients_linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +35,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
        ImageView ingredientsIv =(ImageView)  findViewById(R.id.image_iv);
-
         alsoKnownAsTv=(TextView)findViewById(R.id.also_known_tv);
         ingredientTv=(TextView)findViewById(R.id.ingredients_tv);
+        also_known_linearLayout= (LinearLayout) findViewById(R.id.also_known_layout);
+        ingredients_linearLayout=(LinearLayout)findViewById(R.id.ingredients_layout);
         placeOfOriginTv=(TextView)findViewById(R.id.origin_tv);
         descriptionTv=(TextView)findViewById(R.id.description_tv);
 
@@ -63,8 +69,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
         populateUI();
-        Picasso.with(this)
+        Picasso.get()
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.holder)
+                .error(R.drawable.error)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -76,13 +84,25 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
-        placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
         descriptionTv.setText(sandwich.getDescription());
-        for(int c=0;c<sandwich.getIngredients().size();c++) {
-            ingredientTv.append(sandwich.getIngredients().get(c));
+        if(sandwich.getPlaceOfOrigin().isEmpty()){
+            placeOfOriginTv.setText("NO data found");
+        }else {
+            placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
         }
-        for(int i=0;i<sandwich.getAlsoKnownAs().size();i++){
-            alsoKnownAsTv.append(sandwich.getAlsoKnownAs().get(i));
+        if(sandwich.getIngredients().isEmpty()){
+            ingredients_linearLayout.setVisibility(View.GONE);
+            } else {
+            for (int c = 0; c < sandwich.getIngredients().size(); c++) {
+                ingredientTv.setText(TextUtils.join(" , ",sandwich.getIngredients()));
+            }
         }
+            if(sandwich.getAlsoKnownAs().isEmpty()){
+                also_known_linearLayout.setVisibility(View.GONE);
+              }else {
+                for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i++) {
+                    alsoKnownAsTv.setText(TextUtils.join(" , ", sandwich.getAlsoKnownAs()));
+                }
+            }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -12,26 +12,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOW_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_INGREDIENTS="ingredients";
+    public static final String Key_DESCRIPTION="description";
+    public static final String KEY_NAME="name";
+    public static final String KEY_IMAGE="image";
 
     public static Sandwich parseSandwichJson(String json) throws JSONException {
         Sandwich sandwich= new Sandwich();
         JSONObject sandwichDetail = new JSONObject(json);
-        JSONObject name=sandwichDetail.getJSONObject("name");
-        String sandwichMainName = name.getString("mainName");
+        JSONObject name=sandwichDetail.getJSONObject(KEY_NAME);
+        String sandwichMainName = name.getString(KEY_MAIN_NAME);
 
-        String placeOfOrigin = sandwichDetail.getString("placeOfOrigin");
-        String description=sandwichDetail.getString("description");
-        JSONArray sandwichIngredients=sandwichDetail.getJSONArray("ingredients");
+        String placeOfOrigin = sandwichDetail.getString(KEY_PLACE_OF_ORIGIN);
+        String description=sandwichDetail.getString(Key_DESCRIPTION);
+        JSONArray sandwichIngredients=sandwichDetail.getJSONArray(KEY_INGREDIENTS);
         ArrayList<String> currentSandwichIngredients=new ArrayList<>();
         for(int c=0;c<sandwichIngredients.length();c++){
             currentSandwichIngredients.add(sandwichIngredients.getString(c));
         }
-        JSONArray alsoKnownAs=name.getJSONArray("alsoKnownAs");
+        JSONArray alsoKnownAs=name.getJSONArray(KEY_ALSO_KNOW_AS);
         ArrayList<String> currentAlsoKnownAs=new ArrayList();
         for(int i=0;i<alsoKnownAs.length();i++){
             currentAlsoKnownAs.add(alsoKnownAs.getString(i));
         }
-        String image=sandwichDetail.getString("image");
+        String image=sandwichDetail.getString(KEY_IMAGE);
         sandwich=new Sandwich(sandwichMainName,currentAlsoKnownAs,placeOfOrigin,description,image,currentSandwichIngredients);
         return sandwich;
     }
